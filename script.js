d00fc207eb671e20a58a1b8fea3a0d873e34ed62
Rob B <rvabcode@gmail.com>
2021-08-22 01:25:14 -0400

    added javascript logic for close button and border radius for differnet breakpoints.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const close = document.querySelector('.close');
+const contain = document.querySelector('.container');
 
+function gone() {
+  contain.classList.add('container-gone');
+  console.log(contain);
+}
+
+const bye = close.addEventListener('click', gone);
+console.log(bye);
